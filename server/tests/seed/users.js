38dfa1aa18077fe227a630f340e05a9b5d51993c
thebasix <asamuga@gmail.com>
2016-09-17 17:40:24 +0200

    Add tests for GET /users/me and POST /users

---
+++ server/tests/seed/users.js
@@ -0,0 +1,31 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const {User} = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+const users = [{
+    _id: userOneId,
+    email: 'messi@fcbarcelona.com',
+    password: 'messi10',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+    }]
+}, {
+    _id: userTwoId,
+    email: 'iniesta@fcbarcelona.com',
+    password: 'iniesta8'
+}];
+
+const populateUsers = (done) => {
+    User.remove({}).then(() => {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
+
+        return Promise.all([userOne, userTwo])
+    }).then(() => done());
+};
+
+module.exports = {users, populateUsers};
